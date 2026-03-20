c2c91fa57d814cbd164ae22f41089074aa3b751d
Adam Thomas <thomasadam83@hotmail.com>
2026-03-20 09:49:08 -0400

    Add HTML boilerplate and W3.CSS centered domain lookup form

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>WHOIS Lookup</title>
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+	<div class="w3-display-middle w3-container">
+		<div class="w3-card w3-padding w3-center">
+			<form method="post" action="">
+				<label for="domain" class="w3-large">Enter domain name:</label><br>
+				<input class="w3-input w3-border w3-margin-top" type="text" id="domain" name="domain" required placeholder="example.com">
+				<button class="w3-button w3-blue w3-margin-top" type="submit">Lookup</button>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
