52d82647634ec527cea08521c5f958ba394a6f6a
Adam Thomas <thomasadam83@hotmail.com>
2026-03-20 09:50:33 -0400

    Top-align form using W3.CSS, keep horizontal centering

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 <body>
-	<div class="w3-display-middle w3-container">
-		<div class="w3-card w3-padding w3-center">
+	<div class="w3-container w3-margin-top">
+		<div class="w3-card w3-padding w3-center" style="max-width:400px;margin:auto;">
 			<form method="post" action="">
 				<label for="domain" class="w3-large">Enter domain name:</label><br>
 				<input class="w3-input w3-border w3-margin-top" type="text" id="domain" name="domain" required placeholder="example.com">
